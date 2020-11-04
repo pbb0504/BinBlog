3fe79787c9e8f71df9ebf644bf076b6303361d99
panbinbin <1305355506@qq.com>
2020-11-04 12:34:29 +0800

    reviewVue01

---
+++ BinBlog_F/src/components/Test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="displayElement">
+        <p>
+            this is  a test vue component
+            {{message}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data(){
+        return {
+            message: 'ohhhhhhhhhhhhhhhhhhhhhh'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #displayElement {
+        font-size: 40px;
+        color: red;
+        border-radius: 12px;
+        border: 1px red;
+    }
+</style>
