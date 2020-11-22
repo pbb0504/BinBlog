b738ceee1d624054b8780c9e987065fabdb68c50
panbinbin <1305355506@qq.com>
2020-11-22 23:00:18 +0800

    article test

---
+++ BinBlog_F/src/components/Article.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- 标题 -->
+    <div>
+      {{ title }}
+    </div>
+    <!-- 标签 -->
+    <div>
+      {{ flag }}
+    </div>
+    <!-- 内容 -->
+    <div>
+      {{ content }}
+    </div>
+    <!-- 时间 -->
+    <div>
+      {{ postedtime }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+  data() {
+    return {
+      title: "this is a title",
+      flag: "vue",
+      content: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      postedtime: "2020-02-02",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- BinBlog_F/src/components/Test.vue
+++ BinBlog_F/src/components/Test.vue
@@ -1,28 +1,34 @@
 <template>
-    <div id="displayElement">
-        <p>
-            this is  a test vue component
-            {{message}}
-        </p>
-    </div>
+  <div id="displayElement">
+    <p>
+      this is a test vue component
+      {{ message }}
+    </p>
+
+    <Article />
+  </div>
 </template>
 
 <script>
+import Article from "./Article";
 export default {
-    name: 'Test',
-    data(){
-        return {
-            message: 'ohhhhhhhhhhhhhhhhhhhhhh'
-        }
-    }
-}
+  name: "Test",
+  components: {
+    Article,
+  },
+  data() {
+    return {
+      message: "ohhhhhhhhhhhhhhhhhhhhhh",
+    };
+  },
+};
 </script>
 
 <style scoped>
-    #displayElement {
-        font-size: 40px;
-        color: red;
-        border-radius: 12px;
-        border: 1px red;
-    }
+#displayElement {
+  font-size: 40px;
+  color: red;
+  border-radius: 12px;
+  border: 1px red;
+}
 </style>
